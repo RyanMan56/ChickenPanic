3fcd063cea557f6cda23438e183937dde3feb991
Ryan Jones <RyanMan56@gmail.com>
2016-04-10 19:10:21 +0100

    Started Project

---
+++ desktop/src/com/chronojam/chickenpanic/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chronojam.chickenpanic.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chronojam.chickenpanic.ChickenPanic;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChickenPanic(), config);
+	}
+}
